612464b4bc2ba8adc6842019a8be49b125a66880
Jeffrey Stines <jmstines00@gmail.com>
2025-11-22 18:59:31 -0500

    Update API Gateway deployment triggers to include integration responses for accurate redeployment

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -168,13 +168,15 @@ resource "aws_api_gateway_deployment" "contact_deployment" {
   rest_api_id = aws_api_gateway_rest_api.contact_api.id
 
   triggers = {
-      redeploy = sha1(join("", [
-        aws_api_gateway_method.options.id,
-        aws_api_gateway_method.contact_post.id,
-        aws_api_gateway_integration.options.id,
-        aws_api_gateway_integration.lambda_integration.id
-      ]))
-    }
+    redeploy = sha1(join("", [
+      aws_api_gateway_method.options.id,
+      aws_api_gateway_method.contact_post.id,
+      aws_api_gateway_integration.options.id,
+      aws_api_gateway_integration.lambda_integration.id,
+      aws_api_gateway_integration_response.options.id,
+      aws_api_gateway_integration_response.contact_post.id
+    ]))
+  }
 
   depends_on = [
     aws_api_gateway_integration.lambda_integration,
