421c83f55f9a0d4b0cb2faee68432815a3c4488c
Jeffrey Stines <jmstines00@gmail.com>
2026-01-02 11:44:03 -0500

    Update Terraform deployment to use truncated SHA for S3 key and refactor IAM role configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -22,13 +22,50 @@ data "aws_s3_object" "lambda_zip" {
   key    = var.lambda_code_s3_key
 }
 
-data "aws_iam_role" "lambda_role" {
-  name = "lambda-role"
+resource "aws_iam_role" "lambda_role" {
+  name = "contact-form-lambda-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "lambda_ses" {
+  name = "lambda-ses-policy"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = [
+        "ses:SendEmail",
+        "ses:SendRawEmail"
+      ]
+      Effect   = "Allow"
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_ses" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_ses.arn
 }
 
 resource "aws_lambda_function" "contact_lambda" {
   function_name = var.lambda_function_name
-  role          = data.aws_iam_role.lambda_role.arn
+  role          = aws_iam_role.lambda_role.arn
   handler       = "sendEmailApi.handler"
   runtime       = "nodejs18.x"
 
